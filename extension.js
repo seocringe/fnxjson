9be41f5364d75838e27da8597b78b6001c7d212f
seocringe <privetnambogolet@gmail.com>
2023-11-14 00:24:28 +0000

    Author:    seocringe <privetnambogolet@gmail.com>
     On branch main
    
     Initial commit
    
     Changes to be committed:
    	new file:   .eslintrc.json
    	new file:   .gitignore
    	new file:   .vscode/extensions.json
    	new file:   .vscode/launch.json
    	new file:   .vscodeignore
    	new file:   CHANGELOG.md
    	new file:   README.md
    	new file:   extension.js
    	new file:   jsconfig.json
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   test/runTest.js
    	new file:   test/suite/extension.test.js
    	new file:   test/suite/index.js
    	new file:   vsc-extension-quickstart.md
    
     Changes not staged for commit:
    	modified:   README.md
    	modified:   extension.js
    	modified:   package-lock.json
    	modified:   package.json
     Untracked files:
    	fnxjson.code-workspace

---
+++ extension.js
@@ -0,0 +1,48 @@
+const vscode = require('vscode');
+
+function activate(context) {
+    let disposable = vscode.commands.registerCommand('extension.optimizeJson', async function () {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showInformationMessage('No JSON file is open');
+            return;
+        }
+
+        // Check if the opened file is a JSON file
+        if (editor.document.languageId !== "json") {
+            vscode.window.showErrorMessage('Opened file is not a JSON file');
+            return;
+        }
+
+        const text = editor.document.getText();
+        try {
+            const json = JSON.parse(text);
+
+            // Здесь ваш код для оптимизации JSON (удаление дубликатов, группировка, etc.)
+
+            const optimizedJson = JSON.stringify(json, null, 2);
+            const range = new vscode.Range(
+                editor.document.positionAt(0),
+                editor.document.positionAt(text.length)
+            );
+
+            // Handle the promise returned by edit
+            await editor.edit((editBuilder) => {
+                editBuilder.replace(range, optimizedJson);
+            });
+        } catch (error) {
+            // Show the error message to help with debugging
+            vscode.window.showErrorMessage('Invalid JSON: ' + error.message);
+        }
+    });
+
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+}
